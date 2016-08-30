51ee4bfa90636a9f3909922401bd8af69390d5cf
kamild706 <kamild706@gmail.com>
2016-08-30 20:08:01 +0200

    Add files via upload

---
+++ LexGraphicsPlugin.user.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name        Lex.pl Graphics Plugin
+// @namespace   matt23.lex.graphics.plugin
+// @description Wyświetla grafiki w serwisie Lex
+// @version     1
+// @include     http://*.lex.pl/*
+// @include     https://*.lex.pl/*
+// @grant       none
+// ==/UserScript==
+
+function getRandomInt() {
+  max = 10000000;
+  min = 100000;
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function modifyCssRules(className, content) {
+  re = /(\.\w+\s{)/g;
+  return content.replace(re, "." + className + ' $1');
+}
+
+function extractSVG(data) {
+  return data.substring(data.indexOf('<svg'));
+}
+
+function renderSVGs(item) {
+  $.get(item.href, function(data) {
+    content = extractSVG(data);
+    className = 'graphicsRender' + getRandomInt();
+
+    item.parentNode.className += ' ' + className;
+    svg = modifyCssRules(className, content);
+
+    item.parentNode.innerHTML = svg;
+  });
+};
+
+function doMagic() {
+  $('a[href$="SVG"]').each(function(index) {
+    renderSVGs(this);
+  });
+}
+
+document.addEventListener("keypress", function(event) {
+    doMagic();
+});
